358b99978a73761920730a9a91360fbd86a1a125
SACKO Idriss <imidsac@hotmail.fr>
2015-10-20 11:11:46 +0000

    corrrections.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   ## config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
+  # config.action_controller.asset_host = "//#{ENV['AWS_BUCKET']}.s3.amazonaws.com"
 
 
   # Code is not reloaded between requests.
